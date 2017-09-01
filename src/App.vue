c2a40b1bc4d0e76adab86ac40bc07971f1a5e877
SheepMie <347254621@qq.com>
2017-09-01 17:47:39 +0800

    add tabs

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <!--<router-view></router-view>-->
+    <tabs></tabs>
   </div>
 </template>
 
 <script>
+import Tabs from './components/Tabs.vue'
+
 export default {
-  name: 'app'
+  components: { 'tabs': Tabs }
 }
 </script>
 
--- src/page/index.vue
+++ src/page/index.vue
@@ -1,75 +1,116 @@
 <template>
-    <div class="index_bg">
-        <div class="index_center text-center" >
+        
+        <div class="index_center text-center">
             <div class="index_time">
-                
-                <p>永远别让它熄灭，让火光点亮每一个黑夜。</p>
+                <p>故事的小黄花</p>
 
-<p>多少时间可以让一切被遮掩</p>
+<p>从出生那年就飘着</p>
+<input type="text" @click="aabb">
+<p>童年的荡秋千</p>
 
-<p>你是否依然再相信</p>
+<p>随记忆一直晃到现在</p>
 
-<p>那些曾念念不忘的画面。</p>
+<p>ㄖㄨㄟ ㄙㄡ ㄙㄡ ㄒ一 ㄉㄡ ㄒ一ㄌㄚ</p>
+<p>Re So So Si Do Si La</p>
+<p>ㄙㄡ ㄌㄚ ㄒ一 ㄒ一 ㄒ一 ㄒ一 ㄌㄚ ㄒ一 ㄌㄚ ㄙㄡ</p>
+<p>吹着前奏望着天空</p>
 
-<p>谁在那里安静地听着我的歌，</p>
+<p>我想起花瓣试着掉落</p>
 
-<p>看看今夜的天空，是否像以往一样的美丽</p>
+<p>为你翘课的那一天</p>
 
-<p>嘿！朋友-安子与九妹</p>
+<p>花落的那一天</p>
 
-<p>来吧让我在再你点上一支烟，</p>
+<p>教室的那一间</p>
 
-<p>永远别让它熄灭，让火光点燃每一个黑夜。</p>
+<p>我怎么看不见</p>
 
-<p>谁在那里安静地听着我的歌，永远别让它熄灭，让火光点亮每一个黑夜。</p>
+<p>消失的下雨天</p>
 
-<p>多少时间可以让一切被遮掩</p>
-<input type="text">
-<p>你是否依然再相信</p>
+<p>我好想再淋一遍</p>
 
-<p>那些曾念念不忘的画面。</p>
+<p>没想到失去的勇气我还留着</p>
 
-<p>谁在那里安静地听着我的歌，</p>
+<p>好想再问一遍</p>
 
-<p>看看今夜的天空，是否像以往一样的美丽</p>
+<p>你会等待还是离开</p>
 
-<p>嘿！朋友-安子与九妹</p>
+<p>刮风这天我试过握着你手</p>
 
-来吧让我在再你点上一支烟，
+<p>但偏偏雨渐渐大到我看你不见</p>
 
-永远别让它熄灭，让火光点燃每一个黑夜。
+<p>还要多久我才能在你身边</p>
 
-谁在那里安静地听着我的歌，永远别让它熄灭，让火光点亮每一个黑夜。
+<p>等到放晴的那天也许我会比较好一点</p>
+<p>
+从前从前有个人爱你很久</p>
 
-多少时间可以让一切被遮掩
+<p>
+但偏偏风渐渐把距离吹得好远</p>
 
-你是否依然再相信
+<p>
+好不容易又能再多爱一天</p>
 
-那些曾念念不忘的画面。
+<p>
+但故事的最后你好像还是说了拜拜</p>
 
-谁在那里安静地听着我的歌，
-
-看看今夜的天空，是否像以往一样的美丽
-
-嘿！朋友-安子与九妹
-
-来吧让我在再你点上一支烟，
-
-永远别让它熄灭，让火光点燃每一个黑夜。
-
-谁在那里安静地听着我的歌，</p>
-                
+ 
             </div>
         </div>
-    </div>
 </template>
 
 <script>
+    import moment from 'moment-timezone'
     export default {
-        data () {
+        data(){
             return {
-            msg: 'Welcome to Your Vue.js App'
+                hour: '',
+                minute: '',
+                secondDecade: [],
+                secondUnit: [],
+                toggle: false,
+                toggleTime: true
             }
+        },
+        mounted(){
+            const localToggle = localStorage.getItem('indexToggle');
+            if(localToggle) this.toggle = true;
+            this.getTime();
+            this.startTime = setInterval(this.getTime,500)
+        },
+        methods: {
+            getTime(){
+                const site = this.toggle? "Europe/London":"Asia/Shanghai";
+                var today = moment().tz(site);
+                var h = today.format('H');
+                var m = today.format('m');
+                var s = today.format('s');
+                h = this.checkTime(h);
+                m = this.checkTime(m);
+                s = this.checkTime(s);
+                this.hour = h;
+                this.minute = m;
+                this.secondDecade.pop();
+                this.secondDecade.push(s.toString()[0]);
+                this.secondUnit.pop();
+                this.secondUnit.push(s.toString()[1])
+            },
+            checkTime(i){
+                if (i < 10) {
+                    i = "0" + i
+                }
+                return i
+            },
+            move(){
+                this.toggle = !this.toggle;
+                this.toggle? localStorage.setItem('indexToggle',"1"):localStorage.removeItem('indexToggle');
+            },
+            aabb(){
+                this.toggle = !this.toggle;
+            }
+        },
+        beforeDestroy () {
+            clearInterval(this.startTime);
         }
     }
 </script>
