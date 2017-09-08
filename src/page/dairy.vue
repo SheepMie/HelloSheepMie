8f0e3c29181fa195b9fe68b377a7b01ede5f959e
SheepMie <347254621@qq.com>
2017-09-08 17:49:33 +0800

    change scroll

--- src/page/dairy.vue
+++ src/page/dairy.vue
@@ -1,41 +1,53 @@
 <template>
     <div class="fullheight">
         <pull-to>
-                <dairy-scroller></dairy-scroller>
-            
+            <div class="oflow">
+                <li v-for="item in scrollercont" class="fl pd5" :class="[item.boxsize,item.float]">
+                    <ul  :style="{background: 'url('+item.bgp+')'}">{{item.cont}}</ul>
+                </li>
+            </div>
         </pull-to>
     </div>  
 </template>
 
 <script>
     import PullTo from 'vue-pull-to'
-    import DairyScroller from '../components/DairyScroller.vue'
 
     export default {
         name: 'dairy',
         components: {
-            PullTo,
-             'DairyScroller': DairyScroller
+            PullTo
+        },
+        data () {
+            return {
+                scrollercont:[
+                    {boxsize:'basbox',bgp:'http://img.zcool.cn/community/0049b959b21213a8012028a9b88016.jpg@260w_195h_1c_1e_1o_100sh.jpg',cont:'aiyaya',date:"17-6-20",weither:'cloudy',float:'fl'},
+                    {boxsize:'rowbox',bgp:'http://img.zcool.cn/community/0049b959b21213a8012028a9b88016.jpg@260w_195h_1c_1e_1o_100sh.jpg',cont:'aiyaya',date:"17-6-22",weither:'rain',float:'fr'},
+                    {boxsize:'basbox',bgp:'http://img.zcool.cn/community/0049b959b21213a8012028a9b88016.jpg@260w_195h_1c_1e_1o_100sh.jpg',cont:'aiyaya',date:"17-6-20",weither:'cloudy',float:'fl'},
+                    {boxsize:'colbox',bgp:'http://img.zcool.cn/community/0049b959b21213a8012028a9b88016.jpg@260w_195h_1c_1e_1o_100sh.jpg',cont:'aiyaya',date:"17-6-21",weither:'rain',float:'fl'},
+                    {boxsize:'bigbox',bgp:'http://img.zcool.cn/community/0049b959b21213a8012028a9b88016.jpg@260w_195h_1c_1e_1o_100sh.jpg',cont:'aiyaya',date:"17-6-23",weither:'snow',float:'fl'}
+                ]
+            }
+          },
+        ready() {
+            this.scrollerbox.push()
+        },
+        methods: {
+        plus() {
+        }   
         },
         
     }
 </script>
 <style>
-.sheep-dairy-box{
-    width:100%;
-    height: 100%;
-    display: -webkit-flex; /* Safari */
-    display: flex;
-    flex-wrap:wrap 
-}
-.sheep-dairy-box li {
-    /* background: #fff; */
-    float: left;
-}
 
-.sheep-dairy-box li:nth-child(even) {
-    background: #eee;
-}
+.boxheight1{height: 150px;}
+.boxheight2{height: 300px;}
+.basbox{width:50%;height: 150px;}
+.colbox{width:100%;height: 150px;}
+.rowbox{width:50%;height: 300px;}
+.bigbox{width:100%;height: 300px;}
 
+.oflow li ul{width: 100%;height: 100%;background-repeat: no-repeat !important;background-size: cover !important;overflow: hidden;}
 
 </style>
