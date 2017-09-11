d63ecedd0620651e5a449f7d0a3c715cfbf59eb1
SheepMie <347254621@qq.com>
2017-09-11 17:46:38 +0800

    add velocity

--- src/page/dairy.vue
+++ src/page/dairy.vue
@@ -1,11 +1,15 @@
 <template>
     <div class="fullheight">
         <pull-to>
-            <div class="oflow">
-                <li v-for="item in scrollercont" class="fl pd5" :class="[item.boxsize,item.float]">
-                    <ul  :style="{background: 'url('+item.bgp+')'}">{{item.cont}}</ul>
-                </li>
-            </div>
+             <div class="oflow">
+                 <transition-group  appear  name="staggered-fade" v-bind:css="false" v-on:before-enter="beforeEnter"  v-on:enter="enter" v-on:leave="leave" >
+                    <li v-for="(item,index) in scrollercont" class="fl pd5" :class="[item.boxsize,item.float]" v-bind:key="index+1" v-bind:data-index="index">
+                        
+                            <ul :style="{background: 'url('+item.bgp+')'}">{{item.cont}}</ul>
+                        
+                    </li>
+                </transition-group>
+            </div> 
         </pull-to>
     </div>  
 </template>
@@ -16,25 +20,48 @@
     export default {
         name: 'dairy',
         components: {
-            PullTo
+            PullTo,
         },
         data () {
             return {
+                
                 scrollercont:[
-                    {boxsize:'basbox',bgp:'http://img.zcool.cn/community/0049b959b21213a8012028a9b88016.jpg@260w_195h_1c_1e_1o_100sh.jpg',cont:'aiyaya',date:"17-6-20",weither:'cloudy',float:'fl'},
-                    {boxsize:'rowbox',bgp:'http://img.zcool.cn/community/0049b959b21213a8012028a9b88016.jpg@260w_195h_1c_1e_1o_100sh.jpg',cont:'aiyaya',date:"17-6-22",weither:'rain',float:'fr'},
-                    {boxsize:'basbox',bgp:'http://img.zcool.cn/community/0049b959b21213a8012028a9b88016.jpg@260w_195h_1c_1e_1o_100sh.jpg',cont:'aiyaya',date:"17-6-20",weither:'cloudy',float:'fl'},
-                    {boxsize:'colbox',bgp:'http://img.zcool.cn/community/0049b959b21213a8012028a9b88016.jpg@260w_195h_1c_1e_1o_100sh.jpg',cont:'aiyaya',date:"17-6-21",weither:'rain',float:'fl'},
-                    {boxsize:'bigbox',bgp:'http://img.zcool.cn/community/0049b959b21213a8012028a9b88016.jpg@260w_195h_1c_1e_1o_100sh.jpg',cont:'aiyaya',date:"17-6-23",weither:'snow',float:'fl'}
-                ]
+                    {msg: 'Bruce Lee',boxsize:'basbox',bgp:'http://img.zcool.cn/community/0049b959b21213a8012028a9b88016.jpg@260w_195h_1c_1e_1o_100sh.jpg',cont:'aiyaya',date:"17-6-20",weither:'cloudy',float:'fl'},
+                    {msg: 'Bruce Lee',boxsize:'rowbox',bgp:'http://img.zcool.cn/community/0049b959b21213a8012028a9b88016.jpg@260w_195h_1c_1e_1o_100sh.jpg',cont:'aiyaya',date:"17-6-22",weither:'rain',float:'fr'},
+                    {msg: 'Bruce Lee',boxsize:'basbox',bgp:'http://img.zcool.cn/community/0049b959b21213a8012028a9b88016.jpg@260w_195h_1c_1e_1o_100sh.jpg',cont:'aiyaya',date:"17-6-20",weither:'cloudy',float:'fl'},
+                    {msg: 'Bruce Lee',boxsize:'colbox',bgp:'http://img.zcool.cn/community/0049b959b21213a8012028a9b88016.jpg@260w_195h_1c_1e_1o_100sh.jpg',cont:'aiyaya',date:"17-6-21",weither:'rain',float:'fl'},
+                    {msg: 'Bruce Lee',boxsize:'bigbox',bgp:'http://img.zcool.cn/community/0049b959b21213a8012028a9b88016.jpg@260w_195h_1c_1e_1o_100sh.jpg',cont:'aiyaya',date:"17-6-23",weither:'snow',float:'fl'}
+                ],
             }
           },
         ready() {
             this.scrollerbox.push()
         },
         methods: {
-        plus() {
-        }   
+            beforeEnter: function (el) {
+                el.style.opacity = 0;
+                el.style.scale = 0.5
+            },
+            enter: function (el, done) {
+                var delay = el.dataset.index * 150 //延迟多久开始
+                setTimeout(function () {
+                    Velocity(
+                    el,
+                    { opacity: 1,scale:1  },
+                    { complete: done }
+                    )
+                }, delay)
+            },
+            leave: function (el, done) {
+                var delay = el.dataset.index * 150
+                setTimeout(function () {
+                    Velocity(
+                    el,
+                    { opacity: 0,scale:1 },
+                    { complete: done }
+                    )
+                }, delay)
+            } 
         },
         
     }
@@ -48,6 +75,24 @@
 .rowbox{width:50%;height: 300px;}
 .bigbox{width:100%;height: 300px;}
 
-.oflow li ul{width: 100%;height: 100%;background-repeat: no-repeat !important;background-size: cover !important;overflow: hidden;}
+.oflow li ul{
+    width: 100%;height: 100%;background-repeat: no-repeat !important;background-size: cover !important;overflow: hidden;
+    
+    }
+.vue-waterfall div{background:#f00}
 
+.fade-enter-active,.fade-leave-active{
+  transform-origin:50% 50% 0px;
+  transition: all 0.3s ease-out;
+}
+/* 进入开始 */
+.fade-enter{
+  opacity: 0;
+  transform: scale(0);
+}
+/* 出去终点 */
+.fade-leave-active{
+  opacity: 1;
+  transform: scale(1);
+}
 </style>
