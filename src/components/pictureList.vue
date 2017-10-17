c877f9f9b93c8b8d305331dbea6650f22457c51c
SheepMie <347254621@qq.com>
2017-10-17 17:37:15 +0800

    get swiperchild to template

--- src/components/pictureList.vue
+++ src/components/pictureList.vue
@@ -1,13 +1,59 @@
+<style>
+.gallery-cont{
+  height: 100%;
+}
+.gallery-cont .swiper-slide{
+  width:1000px;
+}
+.gallery-cont .swiper-zoom-container{
+  padding:10px;
+}
+.gallery-cont img{
+  border-radius: 6px;
+}
+.gallery-cont .swiper-slide{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.swiper-slide-active{z-index: 9999}
+</style>
 <template>
-    <div>123</div>
+    <swiper :options="swiperOptionContChild" class="gallery-cont" ref="swiperContChild">
+      <swiper-slide v-for="(item,index) in childMsg" v-bind:key="index+1">
+        <div class="swiper-zoom-container"><img :src="item"></div>
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
 </template>
 
 <script>
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
 export default {
   name: 'picture-list',
+  components: {swiper, swiperSlide},
+  props: ['childMsg'],
   data () {
+    let vm = this;
     return {
 
+        swiperOptionContChild: {
+        spaceBetween: 10,
+        zoom: true,
+        nested:true,
+        resistanceRatio: 0,
+        pagination: '.swiper-pagination',
+        onSlideChangeStart:function(swiper){
+          //vm.backgroundBlur = swiper.activeIndex;
+          if (swiper.zoom.scale != 1) {
+            swiper.zoom.toggleZoom(swiper)
+          }
+        },
+        onTouchStart:function(swiper){
+          vm.$emit('changeSlideChild', swiper)
+        },
+       
+      },
     }
   },
   methods: {
--- src/page/classList.vue
+++ src/page/classList.vue
@@ -1,10 +1,11 @@
 <style scoped>
-.TitleLine{
-    padding:10px;
+
+.TitleLine > div{
+    height: 100%;
 }
-.swiper-container {
+/*.swiper-container {
     overflow: visible;
-}
+}*/
 .fullscreen{
   position: relative;
 }
@@ -71,17 +72,7 @@
 .TitleLine{
   height: calc(100% - 80px);
 }
-.gallery-cont{
-  height: 100%;
-}
-.gallery-cont img{
-  border-radius: 6px;
-}
-.gallery-cont .swiper-slide{
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
+
 
 </style>
 
@@ -90,35 +81,39 @@
       <div class="scroll-cont fullscreen">
           <swiper class="gallery-thumbs" :options="swiperOptionThumbs" ref="swiperThumbs">
             <swiper-slide v-for="(item,index) in classList" v-bind:key="index+1" @click.native="changebox(index)">
-                <li  :style="{'background-image': 'url('+item.bgp+')'}"> <a>{{item.title}}</a></li>
+                <li  :style="{'background-image': 'url('+item.bgp[0]+')'}"> <a>{{item.title}}</a></li>
             </swiper-slide>
           </swiper>
+
           <div class="TitleLine">
-            <swiper :options="swiperOptionCont" class="gallery-cont" ref="swiperCont">
-              <swiper-slide v-for="(item,index) in classList" v-bind:key="index+1"><dt class="swiper-zoom-container"><img :src="item.bgp"></dt></swiper-slide>
+            <swiper :options="swiperOptionCont"   ref="swiperCont">
+              <swiper-slide v-for="(item,index) in classList" v-bind:key="index+1">
+                <picture-list v-on:changeSlideChild='childChange' :child-msg="classList[index].bgp"></picture-list>
+              </swiper-slide>
             </swiper>
         </div>
       </div>
       <div class="fullscreen" style="-webkit-filter: blur(30px);filter: blur(30px);">
-        <div class="background-blur fullscreen" :class="{'back-active':backgroundBlur == index,'back-leave':backgroundBlur !=index}" v-for="(item,index) in classList" :style="{'background-image': 'url('+item.bgp+')'}"></div>  
+        <div class="background-blur fullscreen" :class="{'back-active':backgroundBlur == index,'back-leave':backgroundBlur !=index}" v-for="(item,index) in classList" :style="{'background-image': 'url('+item.bgp[0]+')'}"></div>  
       </div>
     </div>
 </template>
 <script>
     import { swiper, swiperSlide } from 'vue-awesome-swiper'
     import Velocity from 'velocity-animate';
+    import PictureList from '@/components/pictureList.vue';
     export default {
         name: 'classList',
-        components: {Velocity,swiper, swiperSlide},
+        components: {Velocity,swiper, swiperSlide,PictureList},
         data () {
             let vm = this;
             return {
                 classList:[
-                  {title:'相簿',bgp:'https://yanxuan.nosdn.127.net/c646886fdf9abe1c69c4267380137ccc.jpg?imageView&quality=75',topics:[{title:'宁波',id:'001'},{title:'随拍',id:'002'},{title:'四季',id:'003'},{title:'画作',id:'004'}]},
-                  {title:'笔记',bgp:'https://yanxuan.nosdn.127.net/42a74ba5f1579e7f871ce293085c8b62.jpg?imageView&quality=75',topics:[{title:'宁波',id:'001'},{title:'矫情',id:'002'},{title:'大国',id:'003'},{title:'小家',id:'004'}]},
-                  {title:'小说',bgp:'https://yanxuan.nosdn.127.net/48f7fc275398b6cb48aa670e896f6dd6.jpg?imageView&quality=75',topics:[{title:'宁波',id:'001'},{title:'矫情',id:'002'},{title:'大国',id:'003'},{title:'小家',id:'004'}]},
-                  {title:'音乐',bgp:'https://yanxuan.nosdn.127.net/74373b06b38aa134e2df9f8d017370b6.jpg?imageView&quality=75',topics:[{title:'民谣',id:'001'},{title:'外语',id:'002'},{title:'轻音乐',id:'003'}]},
-                  {title:'戒子规',bgp:'https://yanxuan.nosdn.127.net/ce358fcee3a5eb9dba9bce277a7e9445.jpg?imageView&quality=75',topics:[{title:'大道篇',id:'001'},{title:'为人篇',id:'002'},{title:'处事篇',id:'003'}]},
+                  {title:'相簿',bgp:['https://yanxuan.nosdn.127.net/c646886fdf9abe1c69c4267380137ccc.jpg?imageView&quality=75','https://yanxuan.nosdn.127.net/42a74ba5f1579e7f871ce293085c8b62.jpg?imageView&quality=75'],topics:[{title:'宁波',id:'001'},{title:'随拍',id:'002'},{title:'四季',id:'003'},{title:'画作',id:'004'}]},
+                  {title:'笔记',bgp:['https://yanxuan.nosdn.127.net/42a74ba5f1579e7f871ce293085c8b62.jpg?imageView&quality=75'],topics:[{title:'宁波',id:'001'},{title:'矫情',id:'002'},{title:'大国',id:'003'},{title:'小家',id:'004'}]},
+                  {title:'小说',bgp:['https://yanxuan.nosdn.127.net/48f7fc275398b6cb48aa670e896f6dd6.jpg?imageView&quality=75','https://yanxuan.nosdn.127.net/ce358fcee3a5eb9dba9bce277a7e9445.jpg?imageView&quality=75'],topics:[{title:'宁波',id:'001'},{title:'矫情',id:'002'},{title:'大国',id:'003'},{title:'小家',id:'004'}]},
+                  {title:'音乐',bgp:['https://yanxuan.nosdn.127.net/74373b06b38aa134e2df9f8d017370b6.jpg?imageView&quality=75','https://yanxuan.nosdn.127.net/c646886fdf9abe1c69c4267380137ccc.jpg?imageView&quality=75','https://yanxuan.nosdn.127.net/48f7fc275398b6cb48aa670e896f6dd6.jpg?imageView&quality=75',],topics:[{title:'民谣',id:'001'},{title:'外语',id:'002'},{title:'轻音乐',id:'003'}]},
+                  {title:'戒子规',bgp:['https://yanxuan.nosdn.127.net/ce358fcee3a5eb9dba9bce277a7e9445.jpg?imageView&quality=75'],topics:[{title:'大道篇',id:'001'},{title:'为人篇',id:'002'},{title:'处事篇',id:'003'}]},
                 ],
                 swiperOptionThumbs: {
                       slidesPerView: 'auto',
@@ -127,16 +122,16 @@
                  },
                  swiperOptionCont: {
                    spaceBetween: 10,
-                   zoom: true,
                    onSlideChangeStart:function(swiper){
                       vm.backgroundBlur = swiper.activeIndex;
-                      if (swiper.zoom.scale != 1) {
-                        swiper.zoom.toggleZoom(swiper)
+                      if (vm.childSwiperMsg.zoom.scale != 1) {
+                        vm.childSwiperMsg.zoom.toggleZoom(vm.childSwiperMsg)
                       }
-                      
                    }
+
                  },
-                 backgroundBlur:0
+                 
+                 backgroundBlur:0,
             }
           },
           mounted(){
@@ -144,11 +139,13 @@
             const swiperCont = this.$refs.swiperCont.swiper;
             const swiperThumbs = this.$refs.swiperThumbs.swiper;
             swiperCont.params.control = swiperThumbs;
-      
           },
           methods: { 
             changebox (index) {
-              this.$refs.swiperCont.swiper.slideTo(index)
+              this.$refs.swiperCont.swiper.slideTo(index);
+            },
+            childChange(msg) {
+              this.childSwiperMsg = msg;
             }
           }
     }
